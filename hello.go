77a0fbc9a2bc0a4fc730744c52d0abf917750191
faisstzera <alexandre.faisst@gmail.com>
2024-07-12 16:01:17 -0300

    refactor tests and functions

--- hello.go
+++ hello.go
@@ -2,10 +2,33 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
-	return "Hello, " + name
+const spanishLang = "Spanish"
+
+const frenchLang = "French"
+
+const englishHelloPrefix = "Hello, "
+
+const spanishHelloPrefix = "Hola, "
+
+const frenchHelloPrefix = "Bonjour, "
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	prefix := englishHelloPrefix
+
+	switch language {
+	case spanishLang:
+		prefix = spanishHelloPrefix
+	case frenchLang:
+		prefix = frenchHelloPrefix
+	}
+
+	return prefix + name
 }
 
 func main() {
-	fmt.Println(Hello("World"))
+	fmt.Println(Hello("World", ""))
 }

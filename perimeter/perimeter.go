d5b6f6563f78b34d4118a58ccd1e25c577a668fc
faisstzera <alexandre.faisst@gmail.com>
2024-07-29 19:42:12 -0300

    new chapters added

---
+++ perimeter/perimeter.go
@@ -0,0 +1,41 @@
+package peremeter
+
+import "math"
+
+type Shape interface {
+	Area() float64
+}
+
+type Triangle struct {
+	Height float64
+	Base   float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) / 2
+}
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pow(c.Radius, 2) * math.Pi
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
+}
